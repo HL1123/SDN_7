151b3d409d9b46a1e72ff7f28a3a538874eedcd7
HL1123 <1306295598@qq.com>
2020-10-14 21:45:39 +0800

    SDN_work7

---
+++ other.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+
+def http_get(url):
+    url= url
+    headers = {'Content-Type':'application/json'}
+    resp = requests.get(url,headers=headers,auth=HTTPBasicAuth('admin', 'admin'))
+    return resp
+
+
+if __name__ == "__main__":
+    url = 'http://127.0.0.1:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:1/flow-node-inventory:table/0'
+    resp = http_get(url)
+    res = json.loads(resp.content)
+    if res=='{}':
+	print("error")
+    else:
+    	print(resp.content)
+   	print(len(res['flow-node-inventory:table'][0]['flow']))
